ecb232bbf7edce11aab5b7056a824c84d640d22e
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2023-11-16 15:56:57 +0530

    refactor: :poop: wip, go straight to chat

--- src/chat.js
+++ src/chat.js
@@ -2,37 +2,42 @@ const { Document } = require('langchain/document')
 const { RecursiveCharacterTextSplitter } = require('langchain/text_splitter')
 const { openFile } = require('./service/file.js')
 const { embed } = require('./service/embedding.js')
-const { store, search, clearVectorStore } = require('./service/vector.js')
+const {
+  store,
+  search,
+  clearVectorStore,
+  vectorStoreSize
+} = require('./service/vector.js')
 const {
   generate,
   reloadOllama,
   stopOllama,
   runOllama
 } = require('./service/ollama/ollama.js')
-const { exec } = require('child_process')
 
 async function sendChat(event, msg) {
-  try {
+  let prompt = msg
+  if (vectorStoreSize() > 0) {
     const msgEmbeds = await embed([msg])
     const searchResult = search(msgEmbeds[0].embedding, 3)
     //* format the system context search results
     const contextString = searchResult.join('\n\n')
-
-    const prompt = `[INST] Using the provided context, answer the user question to the best of your ability. You must only use information from the provided context. Combine context into a coherent answer.
+    prompt = `Using the provided context, answer the user question to the best of your ability. You must only use information from the provided context. Combine context into a coherent answer.
       If there is nothing in the context relevant to the user question, just say "Hmm, I don't see anything about that in this document." Don't try to make up an answer.
       Anything between the following \`context\` html blocks is retrieved from a knowledge bank, not part of the conversation with the user.
       <context>
         ${contextString}
       <context/>
+      
       If there is no relevant information within the context, just say "Hmm, I don't see anything about that in this document." Don't try to make up an answer. Anything between the preceding 'context' html blocks is retrieved from a knowledge bank, not part of the conversation with the user.
+      
       Anything between the following \`user\` html blocks is is part of the conversation with the user.
       <user>
         ${msg}
-      </user> [/INST]`
-
-    console.log(prompt)
-
-    await generate('llama2', prompt, (json) => {
+      </user>`
+  }
+  try {
+    await generate('mistral', prompt, (json) => {
       //* Reply with the content every time we receive data
       event.reply('chat:reply', { success: true, content: json })
     })
@@ -45,9 +50,18 @@ async function sendChat(event, msg) {
 async function newChat(event) {
   try {
     //* reload the services to clear any previous state
-    // await reloadVectorStore()
     clearVectorStore()
     reloadOllama()
+    event.reply('chat:load', { success: true, content: 'success' })
+  } catch (err) {
+    console.log(err)
+    event.reply('chat:load', { success: false, content: err.message })
+  }
+}
+
+async function loadDocument(event) {
+  try {
+    clearVectorStore()
 
     //* read the document
     const doc = await openFile()
@@ -71,10 +85,10 @@ async function newChat(event) {
     //* store the embeddings
     store(embeddings)
 
-    event.reply('chat:load', { success: true, content: 'success' })
+    event.reply('doc:load', { success: true, content: 'success' })
   } catch (err) {
     console.log(err)
-    event.reply('chat:load', { success: false, content: err.message })
+    event.reply('doc:load', { success: false, content: err.message })
   }
 }
 
@@ -94,6 +108,7 @@ function stopLLM(event) {
 module.exports = {
   newChat,
   sendChat,
+  loadDocument,
   loadLLM,
   stopLLM
 }
